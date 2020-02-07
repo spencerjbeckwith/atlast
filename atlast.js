5ed2b336dab6608e7ac6af7381fb2c273254a0af
Mcwequiesk <spencerjbeckwith@gmail.com>
2020-02-07 00:58:59 -0700

    config and output files

--- atlast.js
+++ atlast.js
@@ -1,32 +1,61 @@
 const fs = require(`fs`);
-const jimp = require(`jimp`);
+const Jimp = require(`jimp`);
 
-function atlastCompile() {
-    //Put all our image frames into one png. Write a json
+const config = JSON.parse(fs.readFileSync(`config.json`));
+let outputObject = {};
+
+function compile() {
+    new Jimp(config.atlasWidth,config.atlasHeight,(error,image) => {
+        //Plop all the images in
+
+        //Save the final Jump to a file
+        image.write(config.outputImageName);
+    });
+    fs.writeFileSync(config.outputJSONName,JSON.stringify(outputObject));
 }
 
-function atlastHelp() {
+function set(key,value) {
+    //Update a value in our config, rewrite the config file
+    if (key === undefined || value === undefined) {
+        throw `Cannot update config: both a key and value must be specified.`;
+    }
+    config[key] = value;
+    fs.writeFileSync(`config.json`,JSON.stringify(config));
+    console.log(`config.json has been updated.`);
+}
+
+function help() {
     //Display help
-    console.log(`atlast by Spencer J. Beckwith. Version ${require("./package.json").version}`);
+    console.log(`atlast by Spencer J. Beckwith. Version ${require(`./package.json`).version}.`);
 }
 
 const args = process.argv.slice(2);
 switch (args[0]) {
-    case ("compile"): {
-        atlastCompile();
+    case (`compile`): {
+        compile();
         break;
     }
-    case ("help"): {
-        atlastHelp();
+    case (`set`): {
+        set(args[1],args[2]);
+        break;
+    }
+    case (`help`): {
+        help();
         break;
     }
     //More commands here
     default: {
         if (args[0] === undefined) {
-            atlastHelp();
+            help();
         } else {
             console.error(`Invalid atlast argument: ${args[0]}`);
         }
         break;
     }
+}
+
+module.exports = {
+    compile: compile,
+    set: set,
+    help: help
 }
